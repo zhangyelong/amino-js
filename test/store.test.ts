322385461edab407ce0fc4339e8badfddbf9f333
zhangyelong <yelong@bianjie.ai>
2019-12-24 15:28:47 +0800

    Fix tests

--- test/store.test.ts
+++ test/store.test.ts
@@ -6,81 +6,80 @@ const txData =
 
 // https://rpc.irisnet.org/txs/8F985BD163782D09C13D145D2099E7EED1ADABE3E0AC58C148CFBE4A05613C34
 const tx = {
-  type: "irishub/bank/StdTx",
-  value: {
-    msg: [
-      {
-        type: "irishub/bank/Send",
-        value: {
-          inputs: [
+    type: "irishub/bank/StdTx",
+    value: {
+        msg: [
             {
-              address: "iaa1n5x9ng3ufr29nw4eauzq6pkwzgkqrxdgacph4t",
-              coins: [
-                {
-                  denom: "iris-atto",
-                  amount: "2860299333300000000000000"
+                type: "irishub/bank/Send",
+                value: {
+                    inputs: [
+                        {
+                            address: "iaa1n5x9ng3ufr29nw4eauzq6pkwzgkqrxdgacph4t",
+                            coins: [
+                                {
+                                    denom: "iris-atto",
+                                    amount: "2860299333300000000000000"
+                                }
+                            ]
+                        }
+                    ],
+                    outputs: [
+                        {
+                            address: "iaa1qvaa9edpprud4k5038ksvtnkjlgwq8p2j8jakm",
+                            coins: [
+                                {
+                                    denom: "iris-atto",
+                                    amount: "2860299333300000000000000"
+                                }
+                            ]
+                        }
+                    ]
                 }
-              ]
             }
-          ],
-          outputs: [
-            {
-              address: "iaa1qvaa9edpprud4k5038ksvtnkjlgwq8p2j8jakm",
-              coins: [
+        ],
+        fee: {
+            amount: [
                 {
-                  denom: "iris-atto",
-                  amount: "2860299333300000000000000"
+                    denom: "iris-atto",
+                    amount: "300000000000000000"
                 }
-              ]
-            }
-          ]
-        }
-      }
-    ],
-    fee: {
-      amount: [
-        {
-          denom: "iris-atto",
-          amount: "300000000000000000"
-        }
-      ],
-      gas: "50000"
-    },
-    signatures: [
-      {
-        pub_key: {
-          type: "tendermint/PubKeySecp256k1",
-          value: "A4IEXMxyBa8QTSyjNii3uxGhnALvBWsKeP2nMcYFd6KE"
+            ],
+            gas: "50000"
         },
-        signature:
+        signatures: [
+            {
+                pub_key: {
+                    type: "tendermint/PubKeySecp256k1",
+                    value: "A4IEXMxyBa8QTSyjNii3uxGhnALvBWsKeP2nMcYFd6KE"
+                },
+                signature:
         "EZzvRytBfsqzlULdF74+eOfgli2dCKQOTTJaA0CNPK0BpPfLQ6glOntpoRLw6k+4Btsp2teNUK+ylRlCigCB4Q==",
-        account_number: "4",
-        sequence: "133"
-      }
-    ],
-    memo: "cp diff"
-  }
+                account_number: "4",
+                sequence: "133"
+            }
+        ],
+        memo: "cp diff"
+    }
 };
 
 describe("Store", () => {
-  describe("decoding", () => {
-    describe("Tx", () => {
-      it("decodes bytes", () => {
-        const bytes = Amino.base64ToBytes(txData);
-        const value = Amino.unmarshalTx(bytes, true);
-        console.log(JSON.stringify(value));
-        expect(value).toMatchObject(tx);
-      });
+    describe("decoding", () => {
+        describe("Tx", () => {
+            it("decodes bytes", () => {
+                const bytes = Amino.base64ToBytes(txData);
+                const value = Amino.unmarshalTx(bytes, true);
+                expect(value).toMatchObject(tx);
+            });
+        });
     });
-  });
 
-  describe("encoding", () => {
-    describe("Tx", () => {
-      it("encodes value", () => {
-        const bytes = Amino.marshalTx(tx, true);
-        const data = Amino.bytesToBase64(bytes);
-        expect(data).toBe(txData);
-      });
+    describe("encoding", () => {
+        describe("Tx", () => {
+            it("encodes value", () => {
+                const bytes = Amino.marshalTx(tx, true);
+                const data = Amino.bytesToBase64(bytes);
+                expect(data).toBe(txData);
+            });
+        });
     });
-  });
 });
